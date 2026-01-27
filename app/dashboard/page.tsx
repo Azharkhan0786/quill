7d43396f6cee3d5a487cfbced1cc7e0c106ae340
Azhar Khan <azharkhan844716@gmail.com>
2026-01-28 02:32:29 +0530

    fix(auth): resolve the aynchronous user retrieval

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { redirect } from 'next/navigation'
 
-const page = () => {
-  return (
-    <div>page</div>
+const Page = async () => {
+  
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
+
+  if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
+
+return (
+    <div>Page</div>
   )
 }
 
-export default page
+export default Page
