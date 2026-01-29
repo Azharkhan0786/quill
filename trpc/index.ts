d0d6ce11fb8f283e4f1cc8b64555e83826db2fed
Azhar Khan <azharkhan844716@gmail.com>
2026-01-29 20:39:21 +0530

    feat(trpc): integrate database access

--- trpc/index.ts
+++ trpc/index.ts
@@ -1,24 +1,9 @@
-// import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
-// import { publicProcedure, router } from '/trpc';
-// import { TRPCError } from '@trpc/server';
 
-// export const appRouter = router({
-//     authcallback:publicProcedure.query(()=>{
-//         const{getUser} = getKindeServerSession();
-//         const user = getUser();
-
-//         if(!user.id || !user.email)
-//             throw new TRPCError({code:'UNAUTHORIZED', message:'User is not authenticated'})
-        
-//     })
- 
-// });
-
-// export type AppRouter = typeof appRouter;
 
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { publicProcedure, router } from './trpc'
 import { TRPCError } from '@trpc/server'
+import { db } from '@/db/index'
 
 export const appRouter = router({
   auth: router({
@@ -26,12 +11,15 @@ export const appRouter = router({
       const { getUser } = getKindeServerSession()
       const user = await getUser()
 
-      if (!user?.id || !user?.email) {
+      if (!user?.id || !user?.email) 
         throw new TRPCError({
           code: 'UNAUTHORIZED',
           message: 'User is not authenticated',
         })
-      }
+
+        //check if the user is in the database
+       
+      
 
       return { success: true }
     }),
